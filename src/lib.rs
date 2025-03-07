56bb0d4a1feada27fb4d6d996a2041d9f279ca20
Aleksandr Provotorov <provotorov@inbox.ru>
2025-03-07 15:46:37 +0300

    users methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod settings;
+
+pub use settings::Settings;
+use tracing::instrument;
+pub mod logger;
+pub mod models;
+mod storage;
+pub use storage::Storage;
+
+use anyhow::Result;
+
+pub struct App {
+    pub settings: Settings,
+    pub storage: Storage,
+}
+impl App {
+    #[instrument]
+    pub async fn new(log_level: tracing::Level) -> Result<Self> {
+        logger::init(log_level);
+        let settings = Settings::init()?;
+        let storage = Storage::new().await?;
+        let res = App {
+            settings,
+            storage,
+        };
+        Ok(res)
+    }
+    pub async fn run(&self) -> Result<()> {
+        Ok(())
+    }
+    pub async fn shutdown(&self) -> Result<()> {
+        self.storage.close().await;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn test_new_app() -> Result<()> {
+        let app = App::new(tracing::Level::DEBUG).await?;
+        app.shutdown().await?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let app = planify_rs::App::new(tracing::Level::DEBUG)?;
-    app.run().await?;
+    let app = planify_rs::App::new(tracing::Level::DEBUG).await?;
+    if let Err(e) = app.run().await {
+        tracing::error!("Ошибка в процессе выполнения программы: {e:?}");
+        app.shutdown().await?;
+    }
     Ok(())
 }
